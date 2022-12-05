d578c11ecd0db4ace5ffa4f58e0b9bd908ca3903
arthurgomespassos <arthurgomespassos74@gmail.com>
2022-12-05 17:39:55 -0300

    Adicionado criação automática da pasta de dados e das pastas de importação e exportação do xml

--- model/m_init.c
+++ model/m_init.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #include "m_init.h"
 #include "m_util.h"
 
 #define qtdDeArquivosDeQuantidade 5
 #define qtdDeArquivosDeTabela 6
-
 
 // armazena o nome dos arquivos que armazenam as quantidades dos itens das tabelas
 char nomesDosArquivosDeQuantidade[qtdDeArquivosDeQuantidade][50] = {
@@ -25,7 +26,23 @@ char nomesDosArquivosDeTabela[qtdDeArquivosDeTabela][50] = {
         caminhoDosDados"fornecedor.bin"
 };
 
+
 void init(void) {
+    // cria a pasta para armazenar os dados se ela não existir
+    DIR *path = opendir(caminhoDosDadosSemBarraNoFinal);
+    if (path) closedir(path);
+    else mkdir(caminhoDosDadosSemBarraNoFinal, S_IRWXU);
+
+    // cria a pasta para exportar o xml
+    DIR *path_2 = opendir(caminhoDosDados"export");
+    if (path_2) closedir(path_2);
+    else mkdir(caminhoDosDados"export", S_IRWXU);
+
+    // cria a pasta para importar o xml
+    DIR *path_3 = opendir(caminhoDosDados"import");
+    if (path_3) closedir(path_3);
+    else mkdir(caminhoDosDados"import", S_IRWXU);
+
     // cria os arquivos que faltam referentes as quantidades
     for (int i = 0; i < qtdDeArquivosDeQuantidade; i++)
         if (!isArquivoExiste(nomesDosArquivosDeQuantidade[i]))
@@ -44,5 +61,5 @@ void deletaTodosOsArquivosBinarios(void) {
 
     for (int i = 0; i < qtdDeArquivosDeTabela; i++)
         if (isArquivoExiste(nomesDosArquivosDeTabela[i]))
-            remove(nomesDosArquivosDeTabela);
+            remove(nomesDosArquivosDeTabela[i]);
 }
--- model/m_util.h
+++ model/m_util.h
@@ -6,6 +6,9 @@
 // caminho relativo para a pasta que contém os dados binarios do programa
 #define caminhoDosDados "../data/"
 
+// caminho relativo para a pasta que contém os dados binarios do programa mas não tem o caractere '/' no seu final
+#define caminhoDosDadosSemBarraNoFinal "../data"
+
 // retorna true se arquivo existir e false se não existir
 bool isArquivoExiste(char *caminhoCompletoDoArquivo);
 
@@ -14,6 +17,5 @@ void resetaArquivoBinario(char *caminhoCompletoDoArquivo);
 
 // abre um arquivo binario em modo de escrita wb e coloca um int com valor 0 nele
 void resetaEColocaZeroEmArquivoBinario(char *caminhoCompletoDoArquivo);
-
 
 #endif //LOCADORA_ULTIMO_M_UTIL_H
